e8771e08bcff0b3a784f38a79cca9a45c4dcf3de
jtimberman <joshua@opscode.com>
2012-10-22 14:35:10 -0700

    release v1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## v1.1.0:
+
+* [COOK-1663] - Configurable ListenAddress based off list of interface
+  names
+* [COOK-1685] - Make default sshd_config value more robust
+
 ## v1.0.0:
 
 * [COOK-1014] - Templates for ssh(d).conf files.
--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs openssh"
-version           "1.0.0"
+version           "1.1.0"
 
 recipe "openssh", "Installs openssh"
 
