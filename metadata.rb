e7b43b42d5d4d6be53314def498dc6060288fb0b
Julian C. Dunn <jdunn@opscode.com>
2013-03-21 14:45:01 -0400

    Added iptables recipe

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,10 @@ description       "Installs openssh"
 version           "1.1.4"
 
 recipe "openssh", "Installs openssh"
+recipe "openssh::iptables", "Set up iptables to allow SSH inbound"
 
 %w{ redhat centos fedora ubuntu debian arch scientific suse }.each do |os|
   supports os
 end
+
+depends "iptables"
