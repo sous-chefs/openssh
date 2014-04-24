d98263f5a1d8459f53764dc97319df57acb081dc
Sean OMeara <someara@opscode.com>
2014-04-23 23:33:37 -0700

    Version bump to 1.3.5 (for development)

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Opscode, Inc.'
 maintainer_email  'cookbooks@opscode.com'
 license           'Apache 2.0'
 description       'Installs openssh'
-version           '1.3.4'
+version           '1.3.5'
 
 recipe 'openssh', 'Installs openssh'
 recipe 'openssh::iptables', 'Set up iptables to allow SSH inbound'
