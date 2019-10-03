307fec26aa2e9e2d108a1c7a83a87d959e9cdf3e
Tim Smith <tsmith84@gmail.com>
2019-10-02 21:55:45 -0700

    Remove deprecated recipe and long_description metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs and configures OpenSSH client and daemon'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.8.0'
-
-recipe 'openssh', 'Installs openssh'
-recipe 'openssh::iptables', 'Set up iptables to allow SSH inbound'
 
 %w(aix amazon arch centos fedora freebsd opensuse opensuseleap oracle redhat scientific smartos suse ubuntu zlinux).each do |os|
   supports os
