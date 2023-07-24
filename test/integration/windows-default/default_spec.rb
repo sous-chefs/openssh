abb71700d811fa1d11118043b526988fd5e917ba
Alban Lefebvre <alefebvre11@bloomberg.net>
2023-07-24 09:15:56 +0000

    Re-adding test with no password auth used

--- test/integration/windows-default/default_spec.rb
+++ test/integration/windows-default/default_spec.rb
@@ -13,10 +13,10 @@ describe ssh_config do
   its('UseRoaming') { should eq 'no' }
 end
 
-# # Attempt to ssh to localhost
-# describe command('ssh -oStrictHostKeyChecking=no -v localhost') do
-#   # No way of actually sshing in without a keypair or password
-#   # but being prompted for an authentication method should be sufficient to
-#   # test that SSH is working as expected, for the most part
-#   its('stderr') { should match 'Next authentication method' }
-# end
+# Attempt to ssh to localhost
+describe command('ssh -oStrictHostKeyChecking=no -oPasswordAuthentication=no -v localhost') do
+  # No way of actually sshing in without a keypair or password
+  # but being prompted for an authentication method should be sufficient to
+  # test that SSH is working as expected, for the most part
+  its('stderr') { should match 'Next authentication method' }
+end
