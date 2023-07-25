ae492d88e24f831786e50b323aee29a93b0e406e
Alban Lefebvre <alefebvre11@bloomberg.net>
2023-07-25 07:52:08 +0000

    Capturing stdout & stderr in diff tests

--- test/integration/windows-default/default_spec.rb
+++ test/integration/windows-default/default_spec.rb
@@ -14,17 +14,24 @@ describe ssh_config do
 end
 
 # Attempt to ssh to localhost
-script = <<-EOF
-  Start-Process ssh "-oStrictHostKeyChecking=no -oPasswordAuthentication=no -v localhost" -NoNewWindow -Wait -RedirectStandardOutput stdOut.log -RedirectStandardError stdErr.log
-  gc stdOut.log
-  cmd.exe /C 'type stdErr.log 1>&2'
-  rm *.log
+script_stderr = <<-EOF
+  Start-Process ssh "-oStrictHostKeyChecking=no -oPasswordAuthentication=no -v localhost" -NoNewWindow -Wait -RedirectStandardOutput stdOut.log -RedirectStandardError stdErr.log; gc stdErr.log; rm *.log
 EOF
 
-describe powershell(script) do
+describe powershell(script_stderr) do
   # No way of actually sshing in without a keypair or password
   # but being prompted for an authentication method should be sufficient to
   # test that SSH is working as expected, for the most part
   its('stdout') { should match /Next authentication method/ }
-  its('stderr') { should match /Next authentication method/ }
+end
+
+script_stdout = <<-EOF
+  Start-Process ssh "-oStrictHostKeyChecking=no -oPasswordAuthentication=no -v localhost" -NoNewWindow -Wait -RedirectStandardOutput stdOut.log -RedirectStandardError stdErr.log; gc stdOut.log; rm *.log
+EOF
+
+describe powershell(script_stdout) do
+  # No way of actually sshing in without a keypair or password
+  # but being prompted for an authentication method should be sufficient to
+  # test that SSH is working as expected, for the most part
+  its('stdout') { should eq '' }
 end
